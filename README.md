31fe000394aa30d3f194d2ae77e61092205494ff
Noah Prail <nprail@filiosoft.com>
2019-05-15 17:05:57 -0400

    chore: iOS version is approaching functional.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# cordova-plugin-stripe-terminal
+# cordova-plugin-stripe-terminal!
 
 ## WIP - [Help Wanted](https://github.com/eventOneHQ/cordova-plugin-stripe-terminal/issues/1)
 
@@ -8,4 +8,3 @@ Cordova Plugin for [Stripe Terminal](https://stripe.com/terminal).
 [![npm](https://img.shields.io/npm/dt/cordova-plugin-stripe-terminal.svg)](https://www.npmjs.com/package/cordova-plugin-stripe-terminal)
 [![npm](https://img.shields.io/npm/dm/cordova-plugin-stripe-terminal.svg)](https://www.npmjs.com/package/cordova-plugin-stripe-terminal)
 [![Slack](https://slack-oss.geteventone.com/badge.svg)](https://slack-oss.geteventone.com)
-
--- src/ios/CDVStripeTerminal.swift
+++ src/ios/CDVStripeTerminal.swift
@@ -1,43 +1,44 @@
 import StripeTerminal
+import Foundation
 
-@objc(CDVStripeTerminal) class CDVStripeTerminal: CDVPlugin, DiscoveryDelegate {
-
-    private let config: DiscoveryConfiguration
-    private var readers: [Reader] = []
+@objc(CDVStripeTerminal) class CDVStripeTerminal: CDVPlugin, DiscoveryDelegate, ReaderDisplayDelegate {
+    private var readers: [Any] = []
+    private var readerObjs: [Reader] = []
+    private var inputOptionsList = [Any]()
+    private var inputPrompts = [Any]()
     
-    init(discoveryConfig: DiscoveryConfiguration) {
-        self.config = discoveryConfig
+    private var discoverCancleable: Cancelable?
+    
+    override init() {
+        print("init CDVStripeTerminal")
         super.init()
     }
-
+    
     @objc(setTokenProvider:)
     func setTokenProvider(command: CDVInvokedUrlCommand) {
-
+        
         print("Cordova Stripe Terminal!!!")
-
+        
         var pluginResult = CDVPluginResult(
             status: CDVCommandStatus_ERROR
         )
-
-        print(command as Any)
-
+        
         let apiUrl = command.arguments[0] as? String ?? ""
-
-        print(apiUrl)
+        
         let apiClient = APIClient(apiUrl: apiUrl)
-
+        
         Terminal.setTokenProvider(apiClient)
-
+        
         pluginResult = CDVPluginResult(
             status: CDVCommandStatus_OK
         )
-
+        
         self.commandDelegate!.send(
-            pluginResult, 
+            pluginResult,
             callbackId: command.callbackId
         )
     }
-
+    
     @objc(discoverReaders:)
     func discoverReaders(command: CDVInvokedUrlCommand) {
         // currently hardcoded into sim mode
@@ -46,27 +47,25 @@ import StripeTerminal
             discoveryMethod: .bluetoothScan,
             simulated: true
         )
-
         
-        let cancleable = Terminal.shared.discoverReaders(
-            config, 
-            delegate: self, 
+        
+        self.discoverCancleable = Terminal.shared.discoverReaders(
+            config,
+            delegate: self,
             completion: { error in
                 print(error as Any)
-
-                let errTxt = error as! String
                 
                 let pluginResult = CDVPluginResult(
                     status: CDVCommandStatus_ERROR,
-                    messageAs: errTxt
+                    messageAs: error?.localizedDescription
                 )
                 self.commandDelegate!.send(
                     pluginResult,
                     callbackId: command.callbackId
                 )
-            }
+        }
         )
-
+        
         let pluginResult = CDVPluginResult(
             status: CDVCommandStatus_OK
         )
@@ -75,24 +74,168 @@ import StripeTerminal
             callbackId: command.callbackId
         )
     }
-
+    
     @objc(getReaders:)
     func getReaders(command: CDVInvokedUrlCommand) {
-
         let pluginResult = CDVPluginResult(
             status: CDVCommandStatus_OK,
             messageAs: self.readers
         )
-
+        
         self.commandDelegate!.send(
             pluginResult,
             callbackId: command.callbackId
         )
     }
-
     func terminal(_ terminal: Terminal, didUpdateDiscoveredReaders readers: [Reader]) {
-        print(readers as Any)
-
-        self.readers = readers
+        print(readers as [Any])
+        
+        var items = [Any]()
+        
+        // convert Reader to a plain object
+        for reader in readers {
+            items.append([
+                "serialNumber": reader.serialNumber,
+                "batteryLevel": reader.batteryLevel as Any,
+                // "deviceType": reader.deviceType as Any,
+                "deviceSoftwareVersion": reader.deviceSoftwareVersion as Any
+                ])
+        }
+        
+        self.readers = items
+        self.readerObjs = readers
+    }
+    
+    @objc(getInputOptions:)
+    func getInputOptions(command: CDVInvokedUrlCommand) {
+        let pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_OK,
+            messageAs: self.inputOptionsList
+        )
+        
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
+    func terminal(_ terminal: Terminal, didRequestReaderInput inputOptions: ReaderInputOptions = []) {
+        let inputOptionsString = Terminal.stringFromReaderInputOptions(inputOptions)
+        
+        self.inputOptionsList.append(inputOptionsString)
+    }
+    
+    
+    @objc(getInputPromts:)
+    func getInputPromts(command: CDVInvokedUrlCommand) {
+        let pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_OK,
+            messageAs: self.inputPrompts
+        )
+        
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
+    func terminal(_ terminal: Terminal, didRequestReaderDisplayMessage inputPrompt: ReaderDisplayMessage) {
+        let inputPromptString = Terminal.stringFromReaderDisplayMessage(inputPrompt)
+        
+        self.inputPrompts.append(inputPromptString)
+    }
+    
+    @objc(connectReader:)
+    func connectReader(command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR
+        )
+        
+        let serialNumber = command.arguments[0] as? String ?? ""
+        let selectReader = self.readerObjs.first(where: { $0.serialNumber == serialNumber })
+        
+        let selectedReader = selectReader as! Reader
+        
+        Terminal.shared.connectReader(selectedReader, completion: { reader, error in
+            if let reader = reader {
+                print("Successfully connected to reader: \(reader.serialNumber)")
+                
+                pluginResult = CDVPluginResult(
+                    status: CDVCommandStatus_OK,
+                    messageAs: "Successfully connected to reader: \(reader.serialNumber)"
+                )
+                
+                self.commandDelegate!.send(
+                    pluginResult,
+                    callbackId: command.callbackId
+                )
+            }
+            else if let error = error {
+                print("errorConnect: \(error.localizedDescription)")
+                
+                pluginResult = CDVPluginResult(
+                    status: CDVCommandStatus_ERROR,
+                    messageAs: error.localizedDescription
+                )
+                
+                self.commandDelegate!.send(
+                    pluginResult,
+                    callbackId: command.callbackId
+                )
+            }
+        })
+    }
+    
+    @objc(collectPayment:)
+    func collectPayment(command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR
+        )
+        
+        let clientSecret = command.arguments[0] as? String ?? ""
+        
+        // get the payment intent
+        let paymentIntent = Terminal.shared.retrievePaymentIntent(clientSecret: clientSecret) { paymentIntent, error in
+            
+            // collect a payment method
+            let cancelable = Terminal.shared.collectPaymentMethod(paymentIntent!, delegate: self) { paymentIntent, error in
+                if let paymentIntent = paymentIntent {
+                    
+                    // process the payment
+                    Terminal.shared.processPayment(paymentIntent) { paymentIntent, error in
+                        if let paymentIntent = paymentIntent {
+                            var pluginResult = CDVPluginResult(
+                                status: CDVCommandStatus_ERROR,
+                                messageAs: paymentIntent.stripeId
+                            )
+                            self.commandDelegate!.send(
+                                pluginResult,
+                                callbackId: command.callbackId
+                            )
+                        }
+                        else if let error = error {
+                            pluginResult = CDVPluginResult(
+                                status: CDVCommandStatus_ERROR,
+                                messageAs: error.localizedDescription
+                            )
+                            self.commandDelegate!.send(
+                                pluginResult,
+                                callbackId: command.callbackId
+                            )
+                        }
+                    }
+                }
+                else if let error = error {
+                    pluginResult = CDVPluginResult(
+                        status: CDVCommandStatus_ERROR,
+                        messageAs: error.localizedDescription
+                    )
+                    self.commandDelegate!.send(
+                        pluginResult,
+                        callbackId: command.callbackId
+                    )
+                }
+            }
+        }
+        
+        
     }
 }
